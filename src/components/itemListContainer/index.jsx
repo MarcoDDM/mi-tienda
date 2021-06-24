da8c36063ee64c9486b19510601e5b7d8defc6aa
kryptonitta <karina.zwenger@live.com.ar>
2021-06-24 00:49:23 -0300

    json

--- src/components/itemListContainer/index.jsx
+++ src/components/itemListContainer/index.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import "./style.css";
+import {ItemList} from "../itemList/index";
 
-export const ItemListContainer=(props)=>{
+export const ItemListContainer=({greetings})=>{
+
+    const[products,setProducts]=useState([])
+
+    useEffect(async () =>{
+        const response = await fetch("./json/productos.json")
+        const json = await response.json()
+        console.log(json)
+    })
+
     return(
-        <h1>{props.greetings}</h1>
+        <div>
+        <h1>{greetings}</h1>
+        <ItemList products={products}/>
+        </div>
     )
 }
